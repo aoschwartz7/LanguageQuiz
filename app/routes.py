c4fcea1d708dfa87e906dc1a627ab310cb11e399
Alec Schwartz <alecschwartz@Alecs-MBP.fritz.box>
2019-11-03 13:45:14 +0100

    Created answer() function and checkAnswer.html template

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,18 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
-from wordBank import startgame, random_word
+from wordBank import startgame, random_word, answer
+#Next steps: create new template for right or wrong answer
 
-@app.route('/')
-@app.route('/quizpage')
+@app.route('/', methods=['GET','POST'])
+@app.route('/quizpage', methods=['GET', 'POST'])
 def quizpage():
+    #load wordbank & shuffle terms:
     startgame()
-    return render_template('quizPage.html', title='Quiz', term= random_word())
+    #render quizpage and present user with initial randomized 'term':
+    if request.method == "GET":
+        return render_template('quizPage.html', title='Quiz', term=random_word())
+    #user enters answer and clicks "Check" button:
+    if request.method == "POST":
+        german_word = request.form['german_word']
+        user_answer = request.form['user_answer']
+        return render_template('checkAnswer.html', title='Quiz', term=german_word, user_answer=user_answer, correct_answer=answer(german_word, user_answer))
--- wordBank.py
+++ wordBank.py
@@ -4,6 +4,8 @@ import random
 wordbank = {}
 wordbankkeys = []
 
+# user_answer = input("type answer here")
+
 #load JSON dictionary as a variable:
 def startgame():
     global wordbank
@@ -11,10 +13,21 @@ def startgame():
     with open('test.json') as json_file:
         wordbank = json.load(json_file)
     wordbankkeys = list(wordbank.keys())
-    random.shuffle(wordbankkeys)
 
 def random_word():
-    return wordbankkeys[0]
+    return wordbankkeys[random.randint(0, len(wordbankkeys))]
+
+# function name: answer
+# inputs aka parameter: german_word, user_answer
+# what it does unless this is obvious: gets correct_answer from wordbank, compares user_answer to correct_answer
+# outputs aka return values: returns True or False
+# this function is called by:
+def answer(german_word, user_answer):
+    correct_answer = wordbank[german_word]
+    if user_answer == correct_answer:
+        return correct_answer
+    else:
+        return correct_answer
 
 if __name__=="__main__":
     startgame()
