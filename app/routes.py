8b72c59cb2e72f6e0cf3db3fde5f9d88cc3b4d43
Alec Schwartz <alecschwartz@Alecs-MBP.fritz.box>
2019-11-18 17:37:14 +0100

    camelCased everything

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,18 @@
 from flask import render_template, request
 from app import app
-from wordBank import startgame, random_word, answer
+from wordBank import startGame, randomWord answer
 
 @app.route('/', methods=['GET','POST'])
-@app.route('/quizpage', methods=['GET', 'POST'])
-def quizpage():
+@app.route('/quizPage', methods=['GET', 'POST'])
+def quizPage():
     #load wordbank:
-    startgame()
+    startGame()
     #render quizpage and present user with initial randomized 'term' via random_word function
     if request.method == "GET":
-        return render_template('quizPage.html', title='Quiz', term=random_word())
+        return render_template('quizPage.html', title='Quiz', term=randomWord())
     #user enters answer and clicks "Check" button to initiate POST request
     #create variables from callable form names in quizPage.html so they can be rendered on checkAnswer.html
     if request.method == "POST":
-        german_word = request.form['german_word']
-        user_answer = request.form['user_answer']
-        return render_template('checkAnswer.html', title='Quiz', term=german_word, user_answer=user_answer, correct_answer=answer(german_word, user_answer))
+        germanWord = request.form['germanWord']
+        userAnswer = request.form['userAnswer']
+        return render_template('checkAnswer.html', title='Quiz', term=germanWord, user_answer=userAnswer, correct_answer=answer(germanWord, userAnswer))
